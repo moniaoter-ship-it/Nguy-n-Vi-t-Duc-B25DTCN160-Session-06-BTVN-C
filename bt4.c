18603efa687d42ea5d30304460cf51ed0bfdd509
moniaoter-ship-it <moniaoter@gmail.com>
2025-10-29 15:37:50 +0700

    Add files via upload

--- bt4.c
+++ bt4.c
@@ -5,7 +5,7 @@ int main() {
         printf("Nhap mot so nguyen tu 1 den 10: ");
         scanf("%d", &n);
         if (n < 1 || n > 10) {
-            printf("Loi! Vui long nhap lai trong khoang tu 1 den 10.\n");
+            printf("Loi Vui long nhap lai trong khoang tu 1 den 10.\n");
         }
     } while (n < 1 || n > 10);
     printf("Bang cuu chuong cua %d:\n", n);
